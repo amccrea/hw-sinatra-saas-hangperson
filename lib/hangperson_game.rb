d5d8fb44b992fbf6ff172c911a7a8e1a17fdd134
Alison McCrea <amccrea@users.noreply.github.com>
2015-11-08 21:15:50 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,12 +4,54 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  #rand_word = HangPersonGame.get_random_word 
 
-  # def initialize()
-  # end
-  
-  def initialize(word)
+
+  def initialize(word)# = rand_word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  attr_accessor :word, :guesses, :wrong_guesses
+  
+  # processes a guess and modifies the instance variables wrong_guesses and guesses accordingly
+  def guess(letter)
+
+    raise ArgumentError, 'Guess is nil' unless not letter.nil?
+    raise ArgumentError, 'Guess is empty' unless not letter.empty?
+    letter.downcase!
+    raise ArgumentError, 'Guess is not a letter' unless letter.match(/^[[:alpha:]]$/) 
+
+    if @word.include? letter
+      if @guesses.include? letter
+        return false
+      else 
+        @guesses += letter
+      end
+    else
+      if @wrong_guesses.include? letter
+        return false
+      else
+        @wrong_guesses += letter
+      end
+    end
+  end
+
+  #returns one of the symbols :win, :lose, or :play depending on the current game state
+  def check_win_or_lose
+     if word_with_guesses == @word
+        return :win
+     elsif @wrong_guesses.length >= 7
+       return :lose  
+     else
+       return :play
+     end
+  end
+
+  #substitutes the correct guesses made so far into the word.
+  def word_with_guesses
+     return @word.gsub(/[^#{@guesses}]/,'-') 
   end
 
   def self.get_random_word
